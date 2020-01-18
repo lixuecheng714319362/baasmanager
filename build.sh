9ede781c9ff19ac56b511011870439343b1f963e
lixuecheng <714319362@qq.com>
2020-01-18 09:30:49 +0800

    commit

--- build.sh
+++ build.sh
@@ -9,15 +9,15 @@ fi
 echo "编译baas-gateway"
 cd $BASE/baas-gateway
 go build --mod=vendor 
-mv baas-gateway $BASE/bin
+#mv baas-gateway $BASE/bin
 echo "编译baas-fabricengine"
 cd $BASE/baas-fabricengine
 go build --mod=vendor
-mv baas-fabricengine $BASE/bin
+#mv baas-fabricengine $BASE/bin
 echo "编译baas-kubeengine"
 cd $BASE/baas-kubeengine
 go build --mod=vendor
-mv baas-kubeengine $BASE/bin
+#mv baas-kubeengine $BASE/bin
 #echo "编译baas-frontend"
 #cd $BASE/baas-frontend
 #rm -rf node_modules && npm install --registry=https://registry.npm.taobao.org
